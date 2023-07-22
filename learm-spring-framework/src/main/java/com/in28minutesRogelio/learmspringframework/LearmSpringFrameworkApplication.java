c53bd88d66ce6121ede7f39d9960f707ff139fef
rogeliocarrilloru <rrcr1975@gmail.com>
2023-07-23 00:59:11 +0200

    Initial commit

---
+++ learm-spring-framework/src/main/java/com/in28minutesRogelio/learmspringframework/LearmSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutesRogelio.learmspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearmSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearmSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ learm-spring-framework/src/test/java/com/in28minutesRogelio/learmspringframework/LearmSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutesRogelio.learmspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearmSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
